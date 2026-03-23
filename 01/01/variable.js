fda6424522e0ab136cda3c934abfeab485ce766b
abuzars3388-lab <abuzar.s3388@gmail.com>
2026-03-23 18:02:02 +0000

    datatypes

---
+++ 01/01/variable.js
@@ -0,0 +1,15 @@
+const accountId = 1445533
+let accountEmail = "abuzar@google.com"
+var accountPassword = "12345"
+accountCity = "karachi"
+let accountSate;
+
+// accountId // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "malir"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountSate])
+ 
---
+++ 01/02/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // use all js code as newer version
+
+// alert( 2+2 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3 ) // code readability should be high
+
+console.log("abuzar")
+
+let name = "abuzar"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number = 2 to power 53
+// bigInt
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined =
+// symbol = unique
+
+// object
+
+console.log(typeof age)
